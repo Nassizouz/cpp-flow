eed623e53f2fae37f457ab586fc58a867d4fb745
Noam Arye Nassi <WubbaLubba@users.noreply.github.com>
2022-12-18 23:45:40 +0200

    composition fixes

--- composition.hpp
+++ composition.hpp
@@ -55,26 +55,24 @@ namespace cppflow::composition
 		template <typename... ArgsTs>
 		decltype(auto) operator() (ArgsTs&&... args) & requires std::is_invocable_v<flow_t<OpTs...>&, reflow_t&, ArgsTs&&...>
 		{
-			return caller(static_cast<reflow_t&>(*this), std::forward<ArgsTs>(args)...);
+			return std::forward<flow_t<OpTs...>&>(caller)(std::forward<reflow_t&>(*this), std::forward<ArgsTs>(args)...);
 		}
 
 		template <typename... ArgsTs>
 		decltype(auto) operator() (ArgsTs&&... args) const & requires std::is_invocable_v<const flow_t<OpTs...>&, const reflow_t&, ArgsTs&&...>
 		{
-			return caller(static_cast<const reflow_t&>(*this), std::forward<ArgsTs>(args)...);
+			return std::forward<const flow_t<OpTs...>&>(caller)(std::forward<const reflow_t&>(*this), std::forward<ArgsTs>(args)...);
 		}
 
 		template <typename... ArgsTs>
 		decltype(auto) operator() (ArgsTs&&... args) && requires std::is_invocable_v<flow_t<OpTs...>&&, reflow_t&&, ArgsTs&&...>
 		{
-			return caller(static_cast<reflow_t&&>(*this), std::forward<ArgsTs>(args)...);
+			return std::forward<flow_t<OpTs...>&&>(caller)(std::forward<reflow_t&&>(*this), std::forward<ArgsTs>(args)...);
 		}
 
 		[[no_unique_address]]
 		flow_t<OpTs...> caller;
 	};
-	template <typename... OpTs>
-	reflow_t(OpTs...) -> reflow_t<std::remove_cvref_t<OpTs>...>;
 
 	static inline constexpr reflow_t<> reflow{};
 }
